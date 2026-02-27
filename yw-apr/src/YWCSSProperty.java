050e0a18d8b38c0bbcd0e15dd6b4f848bbeaf085
Inseo Oh <dhdlstjtr@gmail.com>
2026-02-27 16:07:40 +0900

    [WIP] Create project for annotation processor

---
+++ yw-apr/src/YWCSSProperty.java
@@ -0,0 +1,15 @@
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.TypeElement;
+
+public class YWCSSProperty extends AbstractProcessor {
+
+	@Override
+	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
