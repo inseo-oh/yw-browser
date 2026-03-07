703753eb7f8685e39810e080cdb8f37fc1a89686
Oh Inseo <dhdlstjtr@gmail.com>
2026-03-07 10:50:53 +0900

    Add YWCSSPresentationalHint interface for CSS presentational hints

---
+++ yw/src/main/java/io/github/inseooh/yw/css/YWCSSPresentationalHint.java
@@ -0,0 +1,7 @@
+package io.github.inseooh.yw.css;
+
+import io.github.inseooh.yw.css.om.YWCSSStyleDeclaration;
+
+public interface YWCSSPresentationalHint {
+    public YWCSSStyleDeclaration[] getPresentationalHints();
+}
