c48e7e400d2c8b5d6422f260002f46f23227ade4
Oh Inseo <dhdlstjtr@gmail.com>
2026-03-07 15:14:50 +0900

    YWCSSParser: Add entrypoint for parsing stylesheets

--- yw/src/main/java/io/github/inseooh/yw/css/syntax/YWCSSParser.java
+++ yw/src/main/java/io/github/inseooh/yw/css/syntax/YWCSSParser.java
@@ -3,12 +3,22 @@ package io.github.inseooh.yw.css.syntax;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import java.util.logging.Logger;
 
+import io.github.inseooh.yw.YWSyntaxError;
 import io.github.inseooh.yw.YWUtility;
+import io.github.inseooh.yw.css.YWCSSPropertyDescriptor;
+import io.github.inseooh.yw.css.YWCSSPropertySet;
+import io.github.inseooh.yw.css.om.YWCSSRule;
+import io.github.inseooh.yw.css.om.YWCSSStyleDeclaration;
+import io.github.inseooh.yw.css.om.YWCSSStyleSheet;
+import io.github.inseooh.yw.css.selector.YWCSSComplexSelector;
 import io.github.inseooh.yw.encoding.YWEncoding;
 import io.github.inseooh.yw.encoding.YWIOQueue;
 
 public class YWCSSParser {
+	private static final Logger LOGGER = Logger.getLogger(YWCSSParser.class.getSimpleName());
 
 	/**
 	 * @see <a href=
@@ -54,9 +64,9 @@ public class YWCSSParser {
 	private static String decodeBytes(byte[] bytes) {
 		YWEncoding fallback = determineFallbackEncoding(bytes);
 		YWIOQueue input = new YWIOQueue();
-        for (byte aByte : bytes) {
-            input.push(aByte);
-        }
+		for (byte aByte : bytes) {
+			input.push(aByte);
+		}
 		YWIOQueue output = YWEncoding.decode(input, fallback);
 		return output.itemsToString();
 	}
@@ -89,17 +99,17 @@ public class YWCSSParser {
 	/**
 	 * @see <a href= "https://www.w3.org/TR/css-syntax-3/#parse-a-stylesheet">
 	 *      Relevant section in CSS specification</a>
-	 * @see {@link YWCSSParser#newStylesheetInput}
+	 * @see {@link YWCSSParser#newStyleSheetInput}
 	 */
-	interface StylesheetInput {
+	public interface StyleSheetInput {
 		String decode();
 	}
 
-    static StylesheetInput newStylesheetInput(byte[] input) {
+	public static StyleSheetInput newStyleSheetInput(byte[] input) {
 		return () -> decodeBytes(input);
 	}
 
-	static StylesheetInput newStylesheetInput(String input) {
+	public static StyleSheetInput newStyleSheetInput(String input) {
 		return () -> input;
 	}
 
@@ -107,16 +117,16 @@ public class YWCSSParser {
 	 * NOTE: This stylesheet represents unparsed stylesheet. It's just a wrapper
 	 * around token list for the most part.
 	 */
-	private static class Stylesheet {
+	private static class StyleSheet {
 		YWCSSToken[] value = new YWCSSToken[0];
 		String location = null;
 	}
 
-    /**
+	/**
 	 * @see <a href= "https://www.w3.org/TR/css-syntax-3/#parse-a-stylesheet">
 	 *      Relevant section in CSS specification</a>
 	 */
-	private static Stylesheet parseStylesheet(StylesheetInput sInput, String location) {
+	private static StyleSheet parseStyleSheet(StyleSheetInput sInput, String location) {
 		/* S1 *************************************************************************/
 		ParserInput pInput = newParserInput(sInput.decode());
 
@@ -124,7 +134,7 @@ public class YWCSSParser {
 		YWCSSTokenStream input = new YWCSSTokenStream(pInput.normalizeIntoTokenStream());
 
 		/* S3 *************************************************************************/
-		Stylesheet stylesheet = new Stylesheet();
+		StyleSheet stylesheet = new StyleSheet();
 		stylesheet.location = location;
 
 		/* S4 *************************************************************************/
@@ -132,6 +142,86 @@ public class YWCSSParser {
 
 		/* S5 *************************************************************************/
 		return stylesheet;
+	}
+
+	/**
+	 * @see <a href=
+	 *      "https://www.w3.org/TR/css-syntax-3/#style-rule">
+	 *      Relevant section in CSS specification</a>
+	 */
+	private static YWCSSRule.StyleRule parseStyleRule(YWCSSToken.ASTQualifiedRule rule) throws YWSyntaxError {
+		YWCSSComplexSelector[] selectors;
+		YWCSSTokenStream ts = new YWCSSTokenStream(rule.getPreludeTokens());
+		selectors = YWCSSComplexSelector.parseComplexSelectorList(ts);
+		if (!ts.isEndOfTokens()) {
+			throw new YWSyntaxError();
+		}
+		YWCSSToken[] styleBlockContents = parseStyleBlockContents(newParserInput(rule.getBodyTokens()));
+		List<YWCSSStyleDeclaration> declarations = new ArrayList<>();
+		for (YWCSSToken tk : styleBlockContents) {
+			if (tk instanceof YWCSSToken.ASTDeclaration decl) {
+				YWCSSPropertyDescriptor desc = YWCSSPropertySet.DESCRIPTORS
+						.get(decl.getName().toLowerCase(Locale.ROOT));
+				if (desc == null) {
+					LOGGER.warning("Unrecognized property name " + decl.getName());
+					continue;
+				}
+				Object value;
+				try {
+					value = desc.parse(new YWCSSTokenStream(decl.getValueTokens()));
+				} catch (YWSyntaxError e) {
+					LOGGER.warning("Illegal value for property" + decl.getName());
+					e.printStackTrace();
+					continue;
+				}
+				declarations.add(new YWCSSStyleDeclaration(null, value, decl.isImportant()));
+			} else if (tk instanceof YWCSSToken.ASTAtRule) {
+				continue;
+			} else {
+				LOGGER.warning("Unrecognized token " + tk + "encountered during style rule parsing");
+			}
+		}
+
+		return new YWCSSRule.StyleRule(selectors, declarations.toArray(new YWCSSStyleDeclaration[0]), null);
+	}
+
+	/**
+	 * @see <a href=
+	 *      "https://www.w3.org/TR/2021/CRD-css-syntax-3-20211224/#parse-a-style-blocks-contents">
+	 *      Relevant section in CSS specification</a>
+	 */
+	private static YWCSSToken[] parseStyleBlockContents(ParserInput pInput) {
+		/* S1 *************************************************************************/
+		YWCSSTokenStream input = new YWCSSTokenStream(pInput.normalizeIntoTokenStream());
+
+		/* S2 *************************************************************************/
+		return input.consumeStyleBlockContents();
+	}
+
+	/**
+	 * @see <a href=
+	 *      "https://www.w3.org/TR/css-syntax-3/#parse-a-css-stylesheet">
+	 *      Relevant section in CSS specification</a>
+	 */
+	public static YWCSSStyleSheet parseCSSStyleSheet(StyleSheetInput sInput, String location) {
+		StyleSheet rawSheet = parseStyleSheet(sInput, location);
+
+		YWCSSStyleSheet sheet = new YWCSSStyleSheet();
+		sheet.setLocation(rawSheet.location);
+		List<YWCSSRule.StyleRule> styleRules = new ArrayList<>();
+		for (YWCSSToken token : rawSheet.value) {
+			if (token instanceof YWCSSToken.ASTQualifiedRule qRule) {
+				try {
+					styleRules.add(parseStyleRule(qRule));
+				} catch (YWSyntaxError e) {
+					LOGGER.warning("Ignoring invalid qualified rule");
+					e.printStackTrace();
+				}
+
+			}
+		}
+
+		return sheet;
 	}
 
 	/**
@@ -144,7 +234,7 @@ public class YWCSSParser {
 		YWCSSToken[] normalizeIntoTokenStream();
 	}
 
-    static ParserInput newParserInput(YWCSSToken[] input) {
+	static ParserInput newParserInput(YWCSSToken[] input) {
 		return () -> input;
 	}
 
