31304e98b3d52deb7ceb8bf982b172d960267e33
Oh Inseo <dhdlstjtr@gmail.com>
2026-03-11 16:41:07 +0900

    YWEncoding[Encoder,Decoder]: Add hooks for URL and other specs

--- yw/src/main/java/io/github/inseooh/yw/encoding/YWEncodingDecoder.java
+++ yw/src/main/java/io/github/inseooh/yw/encoding/YWEncodingDecoder.java
@@ -1,7 +1,100 @@
 package io.github.inseooh.yw.encoding;
 
+import java.util.Arrays;
+
 public abstract class YWEncodingDecoder extends YWEncodingProcessor {
 	public YWEncodingDecoder() {
 		super(Kind.DECODER);
+	}
+
+	/**
+	 * @see <a href=
+	 *      "https://encoding.spec.whatwg.org/#utf-8-decode">
+	 *      Relevant section in Encoding specification</a>
+	 */
+	public static YWIOQueue utf8Decode(YWIOQueue ioQueue, YWIOQueue output) {
+		// NOTE: All the step numbers(S#.) are based on spec from when this was
+		// initially written(2026.03.11.)
+
+		// S1.
+		int[] buffer = ioQueue.peek(3);
+
+		// S2.
+		if (Arrays.equals(buffer, new int[] { 0xef, 0xbb, 0xbf })) {
+			ioQueue.read(3);
+		}
+
+		// S3.
+		(new YWUTF8Decoder()).processQueue(ioQueue, output, YWEncodingErrorMode.REPLACEMENT);
+
+		// S4.
+		return output;
+	}
+
+	/**
+	 * @see <a href=
+	 *      "https://encoding.spec.whatwg.org/#utf-8-decode">
+	 *      Relevant section in Encoding specification</a>
+	 */
+	public static YWIOQueue utf8Decode(YWIOQueue ioQueue) {
+		return utf8Decode(ioQueue, new YWIOQueue());
+	}
+
+	/**
+	 * @see <a href=
+	 *      "https://encoding.spec.whatwg.org/#utf-8-decode-without-bom">
+	 *      Relevant section in Encoding specification</a>
+	 */
+	public static YWIOQueue utf8DecodeWithoutBOM(YWIOQueue ioQueue, YWIOQueue output) {
+		// NOTE: All the step numbers(S#.) are based on spec from when this was
+		// initially written(2026.03.11.)
+
+		// S1.
+		(new YWUTF8Decoder()).processQueue(ioQueue, output, YWEncodingErrorMode.REPLACEMENT);
+
+		// S2.
+		return output;
+	}
+
+	/**
+	 * @see <a href=
+	 *      "https://encoding.spec.whatwg.org/#utf-8-decode-without-bom">
+	 *      Relevant section in Encoding specification</a>
+	 */
+	public static YWIOQueue utf8DecodeWithoutBOM(YWIOQueue ioQueue) {
+		return utf8DecodeWithoutBOM(ioQueue, new YWIOQueue());
+	}
+
+	/**
+	 * @return null on failure.
+	 * @see <a href=
+	 *      "https://encoding.spec.whatwg.org/#utf-8-decode-without-bom-or-fail">
+	 *      Relevant section in Encoding specification</a>
+	 */
+	public static YWIOQueue utf8DecodeWithoutBOMOrFail(YWIOQueue ioQueue, YWIOQueue output) {
+		// NOTE: All the step numbers(S#.) are based on spec from when this was
+		// initially written(2026.03.11.)
+
+		// S1.
+		YWEncodingResult potentialError = (new YWUTF8Decoder()).processQueue(ioQueue, output,
+				YWEncodingErrorMode.FATAL);
+
+		// S2.
+		if (potentialError == YWEncodingResult.ERROR) {
+			return null;
+		}
+
+		// S3.
+		return output;
+	}
+
+		/**
+	 * @return null on failure.
+	 * @see <a href=
+	 *      "https://encoding.spec.whatwg.org/#utf-8-decode-without-bom-or-fail">
+	 *      Relevant section in Encoding specification</a>
+	 */
+	public static YWIOQueue utf8DecodeWithoutBOMOrFail(YWIOQueue ioQueue) {
+		return utf8DecodeWithoutBOMOrFail(ioQueue, new YWIOQueue());
 	}
 }
--- yw/src/main/java/io/github/inseooh/yw/encoding/YWEncodingEncoder.java
+++ yw/src/main/java/io/github/inseooh/yw/encoding/YWEncodingEncoder.java
@@ -4,4 +4,90 @@ public abstract class YWEncodingEncoder extends YWEncodingProcessor {
 	public YWEncodingEncoder() {
 		super(Kind.ENCODER);
 	}
+
+	/**
+	 * @see <a href= "https://encoding.spec.whatwg.org/#get-an-encoder">
+	 *      Relevant section in Encoding specification</a>
+	 */
+	public static YWEncodingEncoder getEncoder(YWEncoding encoding) {
+		// NOTE: All the step numbers(S#.) are based on spec from when this was
+		// initially written(2026.03.10.)
+
+		// S1.
+		assert encoding != YWEncoding.REPLACEMENT && encoding != YWEncoding.UTF16_BE && encoding != YWEncoding.UTF16_LE;
+
+		// S2.
+		switch (encoding) {
+			case UTF8:
+				return new YWUTF8Encoder();
+			default:
+				throw new RuntimeException("TODO: Add support for " + encoding + " encoding");
+		}
+	}
+
+	/**
+	 * @return null on success, code point that caused failure on failure.
+	 * @see <a href=
+	 *      "https://encoding.spec.whatwg.org/#encode-or-fail">
+	 *      Relevant section in Encoding specification</a>
+	 */
+	public Integer encodeOrFail(YWIOQueue ioQueue, YWIOQueue output) {
+		// NOTE: All the step numbers(S#.) are based on spec from when this was
+		// initially written(2026.03.10.)
+
+		// S1.
+		YWEncodingResult potentialError = processQueue(output, output, YWEncodingErrorMode.FATAL);
+
+		// S2.
+		// NOTE: We don't have explicit "end-of-queue" item.
+
+		// S3.
+		if (potentialError == YWEncodingResult.ERROR) {
+			return potentialError.getItems()[0];
+		}
+
+		// S4.
+		return null;
+	}
+
+	/**
+	 * @see <a href=
+	 *      "https://encoding.spec.whatwg.org/#encode">
+	 *      Relevant section in Encoding specification</a>
+	 */
+	public static YWIOQueue encode(YWIOQueue ioQueue, YWEncoding encoding, YWIOQueue output) {
+		// NOTE: All the step numbers(S#.) are based on spec from when this was
+		// initially written(2026.03.11.)
+
+		// S1.
+		YWEncodingEncoder encoder = getEncoder(encoding);
+
+		// S2.
+		encoder.processQueue(ioQueue, output, YWEncodingErrorMode.HTML);
+
+		// S3.
+		return output;
+	}
+
+	public static YWIOQueue encode(YWIOQueue ioQueue, YWEncoding encoding) {
+		return encode(ioQueue, encoding, new YWIOQueue());
+	}
+
+	/**
+	 * @see <a href=
+	 *      "https://encoding.spec.whatwg.org/#utf-8-encode">
+	 *      Relevant section in Encoding specification</a>
+	 */
+	public static YWIOQueue utf8Encode(YWIOQueue ioQueue, YWIOQueue output) {
+		return encode(ioQueue, YWEncoding.UTF8, output);
+	}
+
+	/**
+	 * @see <a href=
+	 *      "https://encoding.spec.whatwg.org/#utf-8-encode">
+	 *      Relevant section in Encoding specification</a>
+	 */
+	public static YWIOQueue utf8Encode(YWIOQueue ioQueue) {
+		return utf8Encode(ioQueue, new YWIOQueue());
+	}
 }
