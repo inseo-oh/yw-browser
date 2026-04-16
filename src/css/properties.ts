1b87f7a1eb0a526e23888bf0cfc5051ccfc845cf
Oh Inseo <dhdlstjtr@gmail.com>
2026-04-16 10:58:33 +0900

    css/properties: Add name to PropertyDescriptor and use that instead

--- src/css/properties.ts
+++ src/css/properties.ts
@@ -13,24 +13,28 @@ function getPropertyDescriptor(name: string): PropertyDescriptor {
     }
     return desc;
 }
+export function registerPropertyDescriptor(
+    ...descriptors: PropertyDescriptor[]
+): void {
+    for (const descriptor of descriptors) {
+        PROPERTY_DESCRIPTORS.set(descriptor.name, descriptor);
+    }
+}
 
 export interface PropertyDescriptor {
+    name: string;
     valueParser(ts: TokenStream): unknown;
     inherited: boolean;
     initial(): unknown;
     computedValue(parent: unknown, value: unknown): unknown;
 }
 export class ShorthandPropertyValue {
-    values: Map<string, [unknown, PropertyDescriptor]>;
+    values: Map<PropertyDescriptor, unknown>;
 
     constructor(values: Map<string, unknown>) {
         const newValues = new Map();
         values.forEach((value, property) => {
-            const desc = PROPERTY_DESCRIPTORS.get(property);
-            if (desc === undefined) {
-                throw new Error(`No such property named ${property}`);
-            }
-            newValues.set(property, [value, desc]);
+            newValues.set(getPropertyDescriptor(property), value);
         });
         this.values = newValues;
     }
@@ -39,7 +43,7 @@ export class ShorthandPropertyValue {
             if (value instanceof ShorthandPropertyValue) {
                 value.apply(set);
             } else {
-                const prop = set.list.get(property);
+                const prop = set.list.get(property.name);
                 if (prop === undefined) {
                     throw new Error(`No such property named ${property}`);
                 }
@@ -49,10 +53,7 @@ export class ShorthandPropertyValue {
     }
 }
 export class SideShorthandPropertyDescriptor implements PropertyDescriptor {
-    topPropertyName: string;
-    rightPropertyName: string;
-    bottomPropertyName: string;
-    leftPropertyName: string;
+    name: string;
     topPropertyDescriptor: PropertyDescriptor;
     rightPropertyDescriptor: PropertyDescriptor;
     bottomPropertyDescriptor: PropertyDescriptor;
@@ -61,22 +62,21 @@ export class SideShorthandPropertyDescriptor implements PropertyDescriptor {
     #innerValueParser: PropertyDescriptor["valueParser"];
 
     constructor({
+        name,
         topPropertyName,
         rightPropertyName,
         bottomPropertyName,
         leftPropertyName,
         inherited,
     }: {
+        name: string;
         topPropertyName: string;
         rightPropertyName: string;
         bottomPropertyName: string;
         leftPropertyName: string;
         inherited: boolean;
     }) {
-        this.topPropertyName = topPropertyName;
-        this.rightPropertyName = rightPropertyName;
-        this.bottomPropertyName = bottomPropertyName;
-        this.leftPropertyName = leftPropertyName;
+        this.name = name;
         this.inherited = inherited;
         this.topPropertyDescriptor = getPropertyDescriptor(topPropertyName);
         this.rightPropertyDescriptor = getPropertyDescriptor(rightPropertyName);
@@ -108,28 +108,28 @@ export class SideShorthandPropertyDescriptor implements PropertyDescriptor {
         const res = new Map();
         switch (values.length) {
             case 1:
-                res.set(this.topPropertyName, values[0]!);
-                res.set(this.rightPropertyName, values[0]!);
-                res.set(this.bottomPropertyName, values[0]!);
-                res.set(this.leftPropertyName, values[0]!);
+                res.set(this.topPropertyDescriptor.name, values[0]!);
+                res.set(this.rightPropertyDescriptor.name, values[0]!);
+                res.set(this.bottomPropertyDescriptor.name, values[0]!);
+                res.set(this.leftPropertyDescriptor.name, values[0]!);
                 break;
             case 2:
-                res.set(this.topPropertyName, values[0]!);
-                res.set(this.rightPropertyName, values[1]!);
-                res.set(this.bottomPropertyName, values[0]!);
-                res.set(this.leftPropertyName, values[1]!);
+                res.set(this.topPropertyDescriptor.name, values[0]!);
+                res.set(this.rightPropertyDescriptor.name, values[1]!);
+                res.set(this.bottomPropertyDescriptor.name, values[0]!);
+                res.set(this.leftPropertyDescriptor.name, values[1]!);
                 break;
             case 3:
-                res.set(this.topPropertyName, values[0]!);
-                res.set(this.rightPropertyName, values[1]!);
-                res.set(this.bottomPropertyName, values[2]!);
-                res.set(this.leftPropertyName, values[1]!);
+                res.set(this.topPropertyDescriptor.name, values[0]!);
+                res.set(this.rightPropertyDescriptor.name, values[1]!);
+                res.set(this.bottomPropertyDescriptor.name, values[2]!);
+                res.set(this.leftPropertyDescriptor.name, values[1]!);
                 break;
             case 4:
-                res.set(this.topPropertyName, values[0]!);
-                res.set(this.rightPropertyName, values[1]!);
-                res.set(this.bottomPropertyName, values[2]!);
-                res.set(this.leftPropertyName, values[3]!);
+                res.set(this.topPropertyDescriptor.name, values[0]!);
+                res.set(this.rightPropertyDescriptor.name, values[1]!);
+                res.set(this.bottomPropertyDescriptor.name, values[2]!);
+                res.set(this.leftPropertyDescriptor.name, values[3]!);
                 break;
         }
         return new ShorthandPropertyValue(res);
@@ -142,29 +142,38 @@ export class SideShorthandPropertyDescriptor implements PropertyDescriptor {
             throw new Error("parent must be ShorthandPropertyValue");
         }
         const res = new Map();
-        value.values.forEach(([value, propertyDescriptor], property) => {
-            const parentV = parent.values.get(property);
-            if (parentV === undefined) {
+        value.values.forEach((value, propertyDescriptor) => {
+            const parentValue = parent.values.get(propertyDescriptor);
+            if (parentValue === undefined) {
                 throw new Error(
-                    `parent doesn't have value for property ${property}`,
+                    `parent doesn't have value for property ${propertyDescriptor.name}`,
                 );
             }
             res.set(
-                property,
-                propertyDescriptor.computedValue(parentV[1], value),
+                propertyDescriptor,
+                propertyDescriptor.computedValue(parentValue, value),
             );
         });
         return res;
     }
     initial(): unknown {
         const res = new Map();
-        res.set(this.topPropertyName, this.topPropertyDescriptor.initial());
-        res.set(this.rightPropertyName, this.rightPropertyDescriptor.initial());
         res.set(
-            this.bottomPropertyName,
+            this.topPropertyDescriptor.name,
+            this.topPropertyDescriptor.initial(),
+        );
+        res.set(
+            this.rightPropertyDescriptor.name,
+            this.rightPropertyDescriptor.initial(),
+        );
+        res.set(
+            this.bottomPropertyDescriptor.name,
             this.bottomPropertyDescriptor.initial(),
         );
-        res.set(this.leftPropertyName, this.leftPropertyDescriptor.initial());
+        res.set(
+            this.leftPropertyDescriptor.name,
+            this.leftPropertyDescriptor.initial(),
+        );
         return new ShorthandPropertyValue(res);
     }
 }
